69d991efeed05f01ef118fd59ef575b047f5b0a9
cygon23 <rweyemamu088@gmail.com>
2025-08-18 07:51:09 +0300

    importation of middleware both custom and built in
    Signed-off-by: cygon23 <rweyemamu088@gmail.com>

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.route.js";
 import SubscriptionRouter from "./routes/subscription.router.js";
 import connectToDtabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+//bult in middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscription", SubscriptionRouter);
+
+//custom middleware
+app.use(errorMiddleware);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Subscrption tracker APIii");
