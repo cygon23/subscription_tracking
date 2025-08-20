71c61c67cd09ac2445d0fc2b33dca920354061e2
cygon23 <rweyemamu088@gmail.com>
2025-08-20 09:42:56 +0300

    proper error handling
    Signed-off-by: cygon23 <rweyemamu088@gmail.com>

--- app.js
+++ app.js
@@ -12,26 +12,23 @@ const app = express();
 //bult in middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscription", SubscriptionRouter);
 
-//custom middleware
+//custom error  middleware
 app.use(errorMiddleware);
-
 
 app.get("/", (req, res) => {
   res.send("Welcome to Subscrption tracker APIii");
 });
 
-app.listen(PORT, async () => {
-  console.log(
-    `Subscription Tracker APIi is running on http://localhost:${PORT}`
-  );
-
-  await connectToDtabase();
+connectToDtabase().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
 });
 
 export default app;
